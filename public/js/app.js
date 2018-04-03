488e1f33e1ea548e196c0ab06dfece9b1f241fb0
emilence <lucky48829@gmail.com>
2018-04-03 16:40:42 +0530

    chat

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ var socket = io();
 
 socket.on('connect', function(){
 	console.log('connected to socket');
-})
+});
+
+socket.on('message', function(message){
+	console.log('new message');
+	console.log(message.text);
+});
--- server.js
+++ server.js
@@ -4,8 +4,17 @@ var app = express();
 var http =require('http').Server(app);
 var io =require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
-io.on('connection', function (){
+io.on('connection', function (socket){
 	console.log('user connected via socket.io!');
+	socket.on('message', function(message){
+		console.log('message recieved' +message.text);
+          
+		socket.broadcast.emit('message',message);
+	})
+
+	socket.emit('message', {
+		text: 'welcome to the chat application!'
+	});
 });
 http.listen(PORT, function(){
 	console.log('Server started!');
